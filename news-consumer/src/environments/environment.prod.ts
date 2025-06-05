45a55021c7526780130ebd996476283bf3db3a95
Ramiro <rcrespo808@gmail.com>
2025-06-05 19:03:40 -0400

    Add News API configuration

--- news-consumer/src/environments/environment.prod.ts
+++ news-consumer/src/environments/environment.prod.ts
@@ -1,3 +1,7 @@
 export const environment = {
-  production: true
+  production: true,
+  newsApi: {
+    baseUrl: 'https://api.thenewsapi.com/v1',
+    token: 'f10f4341211f466d82f2e909eb926b12'
+  }
 };

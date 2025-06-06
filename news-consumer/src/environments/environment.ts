d38ea5d03af826143502611076b1b40defdf7b27
Ramiro <rcrespo808@gmail.com>
2025-06-05 22:34:57 -0400

    expose newsApi alias in environments

--- news-consumer/src/environments/environment.ts
+++ news-consumer/src/environments/environment.ts
@@ -2,13 +2,16 @@
 // `ng build` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
+const theNewsApi = {
+  baseUrl: 'https://api.thenewsapi.com/v1',
+  token: 'Hri16CtwXylNBvFV0X6ahbv9niqze2qslvJfQ96U'
+};
+
 export const environment = {
   production: false,
+  newsApi: theNewsApi,
   newsSources: {
-    theNewsApi: {
-      baseUrl: 'https://api.thenewsapi.com/v1',
-      token: 'Hri16CtwXylNBvFV0X6ahbv9niqze2qslvJfQ96U'
-    },
+    theNewsApi,
     newsApiOrg: {
       baseUrl: 'https://newsapi.org/v2',
       token: 'f10f4341211f466d82f2e909eb926b12'
